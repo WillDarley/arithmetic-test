895ed3239dc31e2befe739df11fe04303cd675d5
Ashton Rhodes <16RhodeA@fulford.york.sch.uk>
2022-01-05 10:19:07 +0000

    created random operator

--- arithmetic test/Program.cs
+++ arithmetic test/Program.cs
@@ -11,6 +11,40 @@ namespace arithmetic_test
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the arithmetic test");
-        }
+
+            // creates a random operator
+
+            var rnd = new Random();
+
+            var operation = rnd.Next(1, 5);
+            var number1 = rnd.Next(1, 100);
+            var number2 = rnd.Next(1, 100);
+            int answer;
+            string operatorString;
+
+            switch (operation)
+            {
+                case 1:
+                    answer = number1 + number2;
+                    operatorString = "+";
+                    break;
+                case 2:
+                    answer = number1 - number2;
+                    operatorString = "-";
+                    break;
+                case 3:
+                    answer = number1 * number2;
+                    operatorString = "*";
+                    break;
+                case 4:
+                    answer = number1 / number2;
+                    operatorString = "/";
+                    break;
+                default:
+                    answer = 0;
+                    operatorString = "?";
+                    break;
+            }
+            
     }
 }
